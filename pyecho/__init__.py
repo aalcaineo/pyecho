c3c86f4ff2ff07f31406eb5bc401d3374f771d87
aacalineo <alejandroalcaine@gmail.com>
2023-03-09 15:18:19 +0100

    Package of version 0.0.1.

---
+++ pyecho/__init__.py
@@ -0,0 +1,27 @@
+"""
+-----------------------------------------------------------------------------------------
+py-echo
+-----------------------------------------------------------------------------------------
+A Python package to provide tools for handling echo images in DICOM format built on top 
+of pydicom, NumPy, MatPlotLib and Open-CV.
+
+### Version: 0.0.1
+
+### Author:
+Alejandro Alcaine, Ph.D\
+CoMBA research group
+MESC Working Group on e-Cardiology\
+lalcaine@usj.es
+
+Faculty of Health Sciences\
+University San Jorge\
+Villanueva de Gállego (Zaragoza)\
+Spain
+"""
+
+__version__ = "0.0.1"
+
+import pyecho.echo_io
+import pyecho.echo_error
+import pyecho.echo_aux
+import pyecho.echo
